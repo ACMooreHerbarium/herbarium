93f7c1609ef4c75e22eb6138addfb2e46753d27c
Herrick Brown <115589155+ACMooreHerbarium@users.noreply.github.com>
2023-02-09 11:09:23 -0500

    add Arboretum 360 tiles

---
+++ Herrick/360/Saluda_River_Walk/app-files/data.js
@@ -0,0 +1,145 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010006",
+      "name": "R0010006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0161921957843347,
+        "pitch": 0.21468637674585445,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.576230781969139,
+          "pitch": 0.07908835364930766,
+          "rotation": 5.497787143782138,
+          "target": "1-r0010007"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.683794671905062,
+          "pitch": 0.021292299275188498,
+          "title": "Arundinaria gigantea",
+          "text": "River Cane or Giant Cane. A native bamboo. Very similar to Switch Cane (Arundinaria tecta) so name because bad children used to be told to 'go fetch a switch' with which they'd later be beaten."
+        }
+      ]
+    },
+    {
+      "id": "1-r0010007",
+      "name": "R0010007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31936891833741754,
+          "pitch": 0.14211844863700662,
+          "rotation": 5.497787143782138,
+          "target": "2-r0010008"
+        },
+        {
+          "yaw": -2.9376086762336175,
+          "pitch": 0.11215883424904938,
+          "rotation": 5.497787143782138,
+          "target": "0-r0010006"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5631789866696515,
+          "pitch": -0.07169271529234322,
+          "title": "Hedera helix",
+          "text": "English Ivy and introduced and invasive species. Not native."
+        }
+      ]
+    },
+    {
+      "id": "2-r0010008",
+      "name": "R0010008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.647044428993544,
+        "pitch": -0.08823843097757589,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.217992624010229,
+          "pitch": 0.06312142187599612,
+          "rotation": 0,
+          "target": "1-r0010007"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Saluda_River_Walk",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ Herrick/360/test/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010030",
+      "name": "R0010030",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1341599203653736,
+          "pitch": 0.05616841923622751,
+          "rotation": 0,
+          "target": "1-r0010031"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010031",
+      "name": "R0010031",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-r0010030"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.32545768101723205,
+          "pitch": 0.01631192932246961,
+          "title": "test",
+          "text": "<a href='http://herbarium.biol.sc.edu' target='_blank'><u>THIS WORKS!</u></a><div>https://sernecportal.org/portal/taxa/index.php?tid=51590</div><div>&lt;a href=\"https://sernecportal.org/portal/taxa/index.php?tid=51590\" target=\"_blank\"&gt;test&lt;/a&gt;<br><div><br></div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
